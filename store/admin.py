24a50235f20efdbc5d265e5c0fd92d56e925f25d
Surendhar <surendharal65@gmail.com>
2026-09-21 21:02:19 +0530

    admin configurations for names and slugs

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Category, Product
+
+# admin.site.register(Category)
+
+# admin.site.register(Product)
+
+# create super user -> python manage.py createsuperuser
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
